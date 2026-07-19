1aa35c40dbaf5efe9d08c54c18b38c4052b8d733
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-07-19 00:20:13 -0400

    Added icon

--- app/_components/AppHeader.tsx
+++ app/_components/AppHeader.tsx
@@ -2,11 +2,11 @@
 // TODO: use brand logo here
 export function AppHeader() {
     return (
-        <header className="bg-dark">
-            <nav className="bg-dark border-gray-200 px-4 lg:px-6 py-2.5">
+        <header className="bg-black">
+            <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="/" className="flex items-center">
-                        <img src="/vercel.svg" className="mr-3 h-3 sm:h-4" alt="Skybang Insights Logo" />
+                        <img src="/skybang-icon.svg" className="mr-3 header-logo" alt="Skybang Insights Logo" />
                         <span className="self-center text-xl font-semibold whitespace-nowrap">Skybang Insights</span>
                     </a>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default async function Home() {
   var matchups = await matchupService.getMatchupsForWeek("hardcoded-for-now");
 
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-950 font-sans text-zinc-50">
-      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-black px-16 py-32 sm:items-start">
+    <div className="flex flex-1 flex-col items-center justify-center bg-dark font-sans text-zinc-50">
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-dark px-16 py-32 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-white">
             We have the script.
@@ -19,16 +19,18 @@ export default async function Home() {
             Here's this week's projected results. To see that we never miss, you can use the drop down to see our past predictions.
           </p>
           <WeekSelector/>
-          {
-            matchups.map(m => {
-              return (
-                <MatchupRow 
-                  matchup={m} 
-                  key={m.matchupId} 
-                />
-              )
-            })
-          }
+          <div className="flex flex-row">
+            {
+              matchups.map(m => {
+                return (
+                  <MatchupRow 
+                    matchup={m} 
+                    key={m.matchupId} 
+                  />
+                )
+              })
+            }
+          </div>
         </div>
       </main>
     </div>

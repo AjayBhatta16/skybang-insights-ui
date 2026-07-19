59d4a9a81577309754f426e998845e6846dd2c8d
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-07-18 20:11:39 -0400

    Added marketing text

---
+++ app/_components/WeekSelector.tsx
@@ -0,0 +1,6 @@
+
+export function WeekSelector() {
+    return (
+        <></>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            We have the script.
+          </h1>
+          <p className="max-w-xl text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Here's this week's predictions. To see that we never miss, you can use the drop down to see our past predictions.
+          </p>
+        </div>
+      </main>
+    </div>
   );
 }

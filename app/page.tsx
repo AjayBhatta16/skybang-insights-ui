30e7295e3d2d2028524ce2d3ae03e3080ec05329
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-07-18 20:44:43 -0400

    fixed issues caused by ShadCN init

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-950 font-sans text-zinc-50">
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-black px-16 py-32 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-white">
             We have the script.
           </h1>
-          <p className="max-w-xl text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-xl text-lg leading-8 text-zinc-400">
             Here's this week's projected results. To see that we never miss, you can use the drop down to see our past predictions.
           </p>
         </div>

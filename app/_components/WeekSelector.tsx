d087b8431b094c8308e0c4d77c006cc390e11520
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-07-18 20:35:10 -0400

    Week Selector in progress

--- app/_components/WeekSelector.tsx
+++ app/_components/WeekSelector.tsx
@@ -1,6 +1,27 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export function WeekSelector() {
     return (
-        <></>
+        <Select>
+            <SelectTrigger>
+                <SelectValue placeholder="Select a week" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Weeks</SelectLabel>
+                    <SelectItem value="1">Week 1</SelectItem>
+                    <SelectItem value="2">Week 2</SelectItem>
+                    <SelectItem value="3">Week 3</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
     )
 }

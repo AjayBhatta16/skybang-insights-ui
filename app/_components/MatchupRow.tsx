d1ed86b042dfa008facfe09a268347d440c13230
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-07-18 22:20:15 -0400

    Matchup Row component working

--- app/_components/MatchupRow.tsx
+++ app/_components/MatchupRow.tsx
@@ -1,6 +1,50 @@
+import { CSSProperties } from "react";
+import { Matchup } from "../_models/Matchup";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
 
-export function MatchupRow() {
+export type MatchupRowProps = {
+    matchup: Matchup;
+};
+
+function getTeamStyle(matchup: Matchup, home: boolean = false): CSSProperties {
+    if ((matchup.projectedWinner === 'home') === home) {
+        return {
+            backgroundColor: '#F00',
+            border: '2px solid #00F',
+            color: '#00F',
+        };
+    }
+
+    return {
+        backgroundColor: '#000',
+        border: '2px solid #555',
+        color: '#555',
+    };
+}
+
+export function MatchupRow(props: MatchupRowProps) {
     return (
-        <></>
+        <Card>
+            <CardContent>
+                <div className="flex flex-row items-center">
+                    <div 
+                        style={getTeamStyle(props.matchup)}
+                        className="rounded-lg p-4 mr-2"
+                    >
+                        <h3 className="font-bold text-xl">{props.matchup.awayTeamCode}</h3>
+                    </div>
+                    <h1 className="text-3xl mx-4">@</h1>
+                    <div 
+                        style={getTeamStyle(props.matchup, true)}
+                        className="rounded-lg p-4 ml-2"
+                    >
+                        <h3 className="font-bold text-xl">{props.matchup.homeTeamCode}</h3>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import { WeekSelector } from "./_components/WeekSelector";
+import { MatchupService } from "./_services/MatchupService";
+import { MatchupRow } from "./_components/MatchupRow";
 
-export default function Home() {
+export default async function Home() {
+  var matchupService = new MatchupService();
+
+  var matchups = await matchupService.getMatchupsForWeek("hardcoded-for-now");
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center bg-zinc-950 font-sans text-zinc-50">
       <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-black px-16 py-32 sm:items-start">
@@ -13,6 +19,16 @@ export default function Home() {
             Here's this week's projected results. To see that we never miss, you can use the drop down to see our past predictions.
           </p>
           <WeekSelector/>
+          {
+            matchups.map(m => {
+              return (
+                <MatchupRow 
+                  matchup={m} 
+                  key={m.matchupId} 
+                />
+              )
+            })
+          }
         </div>
       </main>
     </div>
